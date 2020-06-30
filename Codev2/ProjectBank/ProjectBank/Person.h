d47e16b53ab3aee93724bbcf7b243305f6611f15
Abigail Cabascango <51092317+sacabascango1@users.noreply.github.com>
2020-06-30 17:22:21 -0500

    In person.h creat virtual function

--- Codev2/ProjectBank/ProjectBank/Person.h
+++ Codev2/ProjectBank/ProjectBank/Person.h
@@ -1,4 +1,4 @@
-/***********************************************************************
+***********************************************************************
  * Module:  Person.h
  * Author:  Abigail
  * Modified: Saturday, June 13, 2020 21:37:31
@@ -13,40 +13,35 @@
 using namespace std;
 class Person {
 public:
-	Person(string, string, string, int);
+	Person(string, string, int);
 	Person();
 	~Person();
 	string getName(void);
 	void setName(string);
-	string getId(void);
-	void setId(string);
+
 	string getNacionality(void);
 	void setNacionality(string);
 	int getAge(void);
 	void setAge(int);
-	void readDates();
-	
-private:
+	virtual void readDates()=0;
+protected:
 	string name;
-	string id;
 	string nacionality;
 	int age;
 };
-Person::Person(string name, string id, string nacionality, int age) {
+Person::Person(string name, string nacionality, int age) {
 	this->name = name;
-	this->id = id;
 	this->nacionality = nacionality;
 	this->age = age;
 }
 
-Person::Person() { this->name = ""; this->id = ""; this->nacionality = ""; this->age = 0; }
+Person::Person() { this->name = ""; this->nacionality = ""; this->age = 0; }
 Person::~Person() {}
 
 string Person::getName() { return name; }
 void Person::setName(string name) { this->name = name; }
 
-string Person::getId() { return id; }
-void Person::setId(string id) { this->id = id; }
+
 
 string Person::getNacionality() { return nacionality; }
 void Person::setNacionality(string nacionality) { this->nacionality = nacionality; }
@@ -59,8 +54,5 @@ void Person::readDates()
 	InputData<int> putDates;
 	InputData<string> putCI;
 	this->setName(putDates.readString("Ingrese su nombre: "));
-	this->setId(putCI.CI("Ingrese su cedula: "));
-	this->setNacionality(putDates.readString("Ingrese su nacioalidad: "));
-	this->setAge(atoi(putDates.positiveInteger("Ingrese su edad: ").c_str()));
 }
 #endif
