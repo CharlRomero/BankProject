dae3b23a83b2dc93b15c2bb2ddc2950b36bee439
CharlRomero <romeokarlos12@gmail.com>
2020-06-30 20:42:24 -0500

    refactor

--- BankProject/Person.h
+++ BankProject/Person.h
@@ -1,58 +1,37 @@
-***********************************************************************
- * Module:  Person.h
- * Author:  Abigail
- * Modified: Saturday, June 13, 2020 21:37:31
- * Purpose: Declaration of the class Person
- ***********************************************************************/
-
 #if !defined(__Person_h)
 #define __Person_h
+
 #include "Account.h"
 #include <iostream>
-#include"InputData.h"
+#include "Date.h"
+
 using namespace std;
 class Person {
 public:
-	Person(string, string, int);
-	Person();
-	~Person();
-	string getName(void);
-	void setName(string);
+	Person(string name, string nacionality, string age, Date date) {
+		this->name = name;
+		this->nacionality = nacionality;
+		this->age = date.calcAge();
+	}
+	Person() { this->name = ""; this->nacionality = ""; this->age = ""; }
+	~Person() {}
+	string getName() { return name; }
+	void setName(string name) { this->name = name; }
 
-	string getNacionality(void);
-	void setNacionality(string);
-	int getAge(void);
-	void setAge(int);
-	virtual void readDates()=0;
+	string getNacionality() { return nacionality; }
+	void setNacionality(string nacionality) { this->nacionality = nacionality; }
+	string getAge() { return age; }
+	void setAge(string age) { this->age = age; }
+	//virtual void readDates()=0;
 protected:
 	string name;
 	string nacionality;
-	int age;
+	string age;
 };
-Person::Person(string name, string nacionality, int age) {
-	this->name = name;
-	this->nacionality = nacionality;
-	this->age = age;
-}
-
-Person::Person() { this->name = ""; this->nacionality = ""; this->age = 0; }
-Person::~Person() {}
-
-string Person::getName() { return name; }
-void Person::setName(string name) { this->name = name; }
-
-
-
-string Person::getNacionality() { return nacionality; }
-void Person::setNacionality(string nacionality) { this->nacionality = nacionality; }
-
-int Person::getAge() { return age; }
-void Person::setAge(int age) { this->age = age; }
-
-void Person::readDates()
-{
-	InputData<int> putDates;
-	InputData<string> putCI;
-	this->setName(putDates.readString("Ingrese su nombre: "));
-}
+//void Person::readDates()
+//{
+//	InputData<int> putDates;
+//	InputData<string> putCI;
+//	this->setName(putDates.readString("Ingrese su nombre: "));
+//}
 #endif
