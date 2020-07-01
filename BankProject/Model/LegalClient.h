8c6a5078c52c363e0fad7a4d94b5cd576cbae647
CharlRomero <romeokarlos12@gmail.com>
2020-07-01 00:49:05 -0500

    Constructors and destroyer

--- BankProject/Model/LegalClient.h
+++ BankProject/Model/LegalClient.h
@@ -2,26 +2,26 @@
 #if !defined(__Legal_h)
 #define __Legal_h
 #include "Person.h"
-class Legal :public Person
-{
+class LegalClient : public Person {
 public:
-	Legal();
-	string getRUC(void);
-	void setRUC(string);
-	void readDates();
+	//Constructors
+	LegalClient(string id, string name, string nacionality, string age): Person(id, name, nacionality, age) {}
+	LegalClient(string id, string name, string nacionality, string age, string ruc) : Person(id, name, nacionality, age) { this->ruc = ruc; }
+	LegalClient(string ruc) { this->ruc = ruc; }
+	LegalClient() : Person() {}
+	//Destroyer
+	~LegalClient() {}
+	string getRUC() { return this->ruc; }
+	void setRUC(string id) { this->ruc = id; }
+	/*void readDates() {
+		InputData<int> putDates;
+		InputData<string> putCI;
+		this->setName(putDates.readString("Ingrese su nombre: "));
+		this->setRUC(putCI.CI("Ingrese su RUC: "));
+		this->setNacionality(putDates.readString("Ingrese su nacioalidad: "));
+		this->setAge(atoi(putDates.positiveInteger("Ingrese su edad: ").c_str()));
+	}*/
 private:
 	string ruc;
 };
-Legal::Legal() { }
-string Legal::getRUC() { return this->ruc; }
-void Legal::setRUC(string id) { this->ruc = id; }
-void Legal::readDates()
-{
-	InputData<int> putDates;
-	InputData<string> putCI;
-	this->setName(putDates.readString("Ingrese su nombre: "));
-	this->setRUC(putCI.CI("Ingrese su RUC: "));
-	this->setNacionality(putDates.readString("Ingrese su nacioalidad: "));
-	this->setAge(atoi(putDates.positiveInteger("Ingrese su edad: ").c_str()));
-}
 #endif
