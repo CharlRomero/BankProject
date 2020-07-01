2cc562e275a55c8d72578e8ed815cea06b250d35
CharlRomero <romeokarlos12@gmail.com>
2020-06-30 23:11:20 -0500

    virtual funtion added

--- BankProject/Controller/BankStream.h
+++ BankProject/Controller/BankStream.h
@@ -1,8 +1,29 @@
-/***********************************************************************
- * Module:  Banca.h
- * Author:  Abigail
- * Modified: Saturday, June 13, 2020 21:35:23
- * Purpose: Declaration of the class Banca
- ***********************************************************************/
+#if !defined(__ModeloBancario_Banca_h)
+#define __ModeloBancario_Banca_h
 
+#include "..\BankProject\Model\Person.h"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <io.h>
+
+using namespace std;
+
+class BankStream {
+public:
+    //Constructors
+    BankStream(string path) { this->path = path; };
+    BankStream() {}
+    //destroyer
+    ~BankStream() {}
+    //Getter and setter of path
+    string getPath() { return path; }
+    void setPath(string path) { this->path = path; }
+
+    virtual void writeFile(vector<Person> customer) = 0;
+    virtual vector<Person> getData() = 0;
+
+private:    
+    string path;
+};
 #endif
