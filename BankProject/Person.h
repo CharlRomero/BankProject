38698e2f106bc9168f47e90fde4e863ea306b2d3
CharlRomero <romeokarlos12@gmail.com>
2020-06-30 21:50:35 -0500

    id attribute added

--- BankProject/Person.h
+++ BankProject/Person.h
@@ -8,22 +8,37 @@
 using namespace std;
 class Person {
 public:
-	Person(string name, string nacionality, string age, Date date) {
-		this->name = name;
+	//Constructors
+	Person(string id, string name, string nacionality, string age, Date date) {
+		this->id = id;
+		this->name = name;		
 		this->nacionality = nacionality;
 		this->age = date.calcAge();
 	}
+	Person(string id, string name, string nacionality, string age) {
+		this->id = id;
+		this->name = name;
+		this->nacionality = nacionality;
+		this->age = age;
+	}
 	Person() { this->name = ""; this->nacionality = ""; this->age = ""; }
+	//Destroyer
 	~Person() {}
+	//Getter and setter of id
+	string getId() { return id; }
+	void setId(string id) { this->id = id; }
+	//Getter and setter of name
 	string getName() { return name; }
 	void setName(string name) { this->name = name; }
-
+	//Getter and setter of nacionality
 	string getNacionality() { return nacionality; }
 	void setNacionality(string nacionality) { this->nacionality = nacionality; }
+	//Getter and setter of age
 	string getAge() { return age; }
-	void setAge(string age) { this->age = age; }
+	void setAge(string age) { this->age = age; }	
 	//virtual void readDates()=0;
 protected:
+	string id;
 	string name;
 	string nacionality;
 	string age;
